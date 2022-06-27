7867aba78f645bbc57d3b0a0fdefc536d4e74aba
Viktoriia <grishulyushenka@gmail.com>
2022-06-27 21:27:24 +0300

    Components and test for RegistrationForm with PageObject added.

--- src/test/java/vgromova/qa/tests/TestBase.java
+++ src/test/java/vgromova/qa/tests/TestBase.java
@@ -2,8 +2,11 @@ package vgromova.qa.tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import vgromova.qa.pages.StudentRegistrationFormPage;
 
 public class TestBase {
+
+    StudentRegistrationFormPage reg_form = new StudentRegistrationFormPage();
 
     @BeforeAll
     static void beforeAll() {
